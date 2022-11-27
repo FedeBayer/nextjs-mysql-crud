c38f99f5b816ff051e90ee626f8f6010e27b4177
FedeBayer <fbayerque@alumnos.exa.unicen.edu.ar>
2022-11-27 10:52:09 -0300

    Added add product

--- components/ProductForm.js
+++ components/ProductForm.js
@@ -1,26 +1,33 @@
 import axios from "axios";
+import { useState } from "react";
 export function ProductForm() {
+  const [product, setProduct] = useState({
+    name: "",
+    description: "",
+    price: 0,
+  });
+
   const handleSubmit = async (e) => {
     e.preventDefault();
-    const res = await axios.post("/api/products", {
-      name: "product1",
-      description: "Product description 1",
-      price: 1000,
-    });
+    const res = await axios.post("/api/products", product);
     console.log(res);
+  };
+
+  const handleChange = ({ target: { name, value } }) => {
+    setProduct({ ...product, [name]: value });
   };
 
   return (
     <div className="bg-gray-300">
       <form onSubmit={handleSubmit}>
         <label htmlFor="name">Name:</label>
-        <input type="text" name="name" />
+        <input type="text" name="name" onChange={handleChange} />
 
         <label htmlFor="price">Price:</label>
-        <input type="text" name="price" />
+        <input type="text" name="price" onChange={handleChange} />
 
         <label htmlFor="description">Description:</label>
-        <input type="text" name="description" />
+        <input type="text" name="description" onChange={handleChange} />
 
         <button type="submit">Save product</button>
       </form>
